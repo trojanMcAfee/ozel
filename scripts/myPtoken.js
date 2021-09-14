d43561f76a09ddc0c9b3c19127b6a122bf452d63
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-09-14 20:14:21 +0200

    For review

--- scripts/myPtoken.js
+++ scripts/myPtoken.js
@@ -10,23 +10,21 @@ const { Wallet, providers: {JsonRpcProvider} } = require('ethers');
     
 async function main() {
 
-  const options = {
-    projectId: 'ead605dd65704007ae941fffb7c1d1a7'
-  };
+  // const options = {
+  //   projectId: 'ead605dd65704007ae941fffb7c1d1a7'
+  // };
 
   // const { constants } = pkg;
-  // const provider = await hre.ethers.provider;
+  const provider = await hre.ethers.provider;
 
-  const provider = new JsonRpcProvider(process.env.ROPSTEN_URL);
-  // const provider = await ethers.getDefaultProvider(process.env.ROPSTEN_URL, options);
-  // const provider = await ethers.getDefaultProvider();
-  const wallet = new Wallet(process.env.PK, provider);
-  const signerAddr = await wallet.address;
+  // const provider = new JsonRpcProvider(process.env.ROPSTEN_URL);
+  // const wallet = new Wallet(process.env.PK, provider);
+  // const signerAddr = await wallet.address;
 
   // const signerAddr = await provider.listAccounts();
   // const signer = await provider.getSigner();
   // const signerAddr = await signer.getAddress();
-  console.log('signer address: ', signerAddr);
+  // console.log('signer address: ', signerAddr);
   
   console.log(provider);
  
@@ -38,28 +36,28 @@ async function main() {
   //   ethProvider: provider,
   // });
 
-  const pbtc = new pBTC({
-      hostBlockchain: 'ETH',
-      hostNetwork: 'testnet_ropsten',
-      nativeBlockchain: 'BTC',
-      nativeNetwork: 'testnet',
-      defaultNode: 'https://pbtconeth-testnet-1a.ngrok.io',
-      ethProvider: provider,
-      ethPrivateKey: process.env.PK
-  });
+  // const pbtc = new pBTC({
+  //     hostBlockchain: 'ETH',
+  //     hostNetwork: 'testnet_ropsten',
+  //     nativeBlockchain: 'BTC',
+  //     nativeNetwork: 'testnet',
+  //     defaultNode: 'https://pbtconeth-testnet-1a.ngrok.io',
+  //     ethProvider: process.env.ROPSTEN_URL
+  //     // ethPrivateKey: process.env.PK
+  // });
   
-  console.log('hi');
-  const depositAddress = await pbtc.getDepositAddress(signerAddr);
-  console.log('BTC deposit address: ', depositAddress.value);
+  // console.log('hi');
+  // const depositAddress = await pbtc.getDepositAddress(signerAddr);
+  // console.log('BTC deposit address: ', depositAddress.value);
 
 
-  depositAddress.waitForDeposit()
-    .once('nativeTxBroadcasted', tx => console.log('nativeTxBroadcasted: ', tx))
-    .once('nativeTxConfirmed', tx => console.log('nativeTxConfirmed: ', tx))
-    .once('nodeReceivedTx', tx => console.log('nodeReceivedTx: ', tx))
-    .once('nodeBroadcastedTx', tx => console.log('nodeBroadcastedTx: ', tx))
-    .once('hostTxConfirmed', tx => console.log('hostTxConfirmed: ', tx))
-    .then(res => console.log('res: ', res));
+  // depositAddress.waitForDeposit()
+  //   .once('nativeTxBroadcasted', tx => console.log('nativeTxBroadcasted: ', tx))
+  //   .once('nativeTxConfirmed', tx => console.log('nativeTxConfirmed: ', tx))
+  //   .once('nodeReceivedTx', tx => console.log('nodeReceivedTx: ', tx))
+  //   .once('nodeBroadcastedTx', tx => console.log('nodeBroadcastedTx: ', tx))
+  //   .once('hostTxConfirmed', tx => console.log('hostTxConfirmed: ', tx))
+  //   .then(res => console.log('res: ', res));
 
   // const tx = await depositAddress.waitForDeposit();
 
