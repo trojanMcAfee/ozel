a62de51b8294961f23bbb0f53383688e230eba6a
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-12-30 22:23:51 +0100

    Refactoring

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,16 +1,14 @@
-const axios = require('axios');
-const bitcore = require('bitcore-lib');
-
 const bitcoin = require('bitcoinjs-lib');
 const zmq = require('zeromq');
-
 const Redis = require('redis');
-
+const Client = require('bitcoin-core');
 require('dotenv').config();
 
 
+const bitPayAddr = 'tb1q0wgk0sf8ucsvh8kmhj4ynnum8pse98mkw45rpl';
+const testAddress = 'mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys';
+
 async function run() {
-    const testAddress = 'mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys';
     const checkOnRedis = async (address) => await redisClient.get(address);
     
     const redisClient = Redis.createClient();
@@ -18,7 +16,7 @@ async function run() {
     await redisClient.connect();
     if (!checkOnRedis(testAddress)) {
         await redisClient.set(testAddress, 1);
-        console.log('Saved on Redis');
+        console.log(testAddress + ' saved on Redis');
     }
 
     sendBitcoinCore(testAddress);
@@ -46,132 +44,24 @@ async function run() {
             }
         }
     }
-
 }
 run();
 
 
-async function tryRedis() {
-    const redisClient = Redis.createClient();
-    redisClient.on('error', (err) => console.log('Redis client error ', err));
-    await redisClient.connect();
-
-    await redisClient.set('mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys', 1);
-    // await redisClient.hSet('a', 'b', 'c');
-    console.log('Saved on Redis');
-
-    // const value = await redisClient.hmGet('a', 'b');
-    const value = await redisClient.get('mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys');
-    console.log('value: ', value);
-}
-// tryRedis();
-
-
-async function listenFor() {
-    const NETWORK = 'BTCTEST';
-    const ADDRESS = 'mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys';
-
-    // const balance = await axios.get(
-    //     `https://sochain.com/api/v2/get_address_balance/${NETWORK}/${ADDRESS}`
-    // );
-    // console.log('balance: ', balance.data.data.confirmed_balance);
-
-    setInterval(async () => {
-        const balance = await axios.get(
-            `https://sochain.com/api/v2/get_address_balance/${NETWORK}/${ADDRESS}`
-        );
-        console.log('balance: ', balance.data.data.confirmed_balance);
-    }, 5000);
-
-}
-// listenFor();
 
 async function sendBitcoinCore(receiverAddr) {
-    const bitPayAddr = 'tb1q0wgk0sf8ucsvh8kmhj4ynnum8pse98mkw45rpl';
-
-    const Client = require('bitcoin-core');
-    const client = new Client({ 
+    const bitcoinClient = new Client({ 
         network: 'testnet', 
         username: 'rulmias', 
         password: 'neQFZWZk@X79rwschXxvq3NwCBKzfMec3', 
         port: 18332 
     });
-
-    const data = await client.sendToAddress(receiverAddr, 0.0003);
+    const data = await bitcoinClient.sendToAddress(receiverAddr, 0.0003);
 }
 
 
 
 
-async function sendBitcoin(receiverAddr, amountToSend, sendingAddr, senderPK) {
-    const sochain_network = "BTCTEST";
-    const decimals = 100000000;
-    let fee = 0;
-    let inputCount = 0;
-    let outputCount = 2;
-    const satoshisToSend = amountToSend * decimals;
-    const satsXfee = 20;
 
-    console.log('hello');
-    // const utxos = await axios.get(
-    //     `https://sochain.com/api/v2/get_tx_unspent/${sochain_network}/${sendingAddr}`
-    // );
-    const utxos = await axios.get(
-        'http://127.0.0.1:8333/api/BTC/testnet/address/mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys/balance'
-    );
-    console.log('hello2');
-
-    const transaction = new bitcore.Transaction();
-    let totalAmountAvailable = 0;
-    let inputs = [];
-    utxos.data.data.txs.forEach(async (element) => {
-        let utxo = {};
-
-        utxo.satoshis = Math.floor(Number(element.value) * decimals);
-        utxo.script = element.script_hex;
-        utxo.address = utxos.data.data.address;
-        utxo.txId = element.txid;
-        utxo.outputIndex = element.output_no;
-
-        totalAmountAvailable += utxo.satoshis;
-        inputCount++;
-        inputs.push(utxo);
-    });
-
-    transaction.from(inputs);
-
-    const transactionSize = inputCount * 180 + outputCount * 34 + 10 - inputCount;
-    fee = transactionSize * satsXfee;
-
-    if (totalAmountAvailable - satoshisToSend < 0) {
-        throw new Error('Balance is too low for this transaction');
-    }
-
-    transaction.to(receiverAddr, satoshisToSend);
-    transaction.fee(fee); 
-    transaction.change(sendingAddr);
-    transaction.sign(senderPK);
-    const serializedTx = transaction.serialize();
-
-    const result = await axios({
-        method: 'POST',
-        // url: `https://sochain.com/api/v2/send_tx/${sochain_network}`,
-        url: 'http://127.0.0.1:8333',
-        data: {
-            tx_hex: serializedTx
-        }
-    });
-
-    console.log('the result: ', result.data.data);
-    return result.data.data;
-}
-
-const bitPayAddr = 'tb1q0wgk0sf8ucsvh8kmhj4ynnum8pse98mkw45rpl';
-// sendBitcoin(bitPayAddr, 0.0001, 'mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys', process.env.PK_TEST);
-
-
-// module.exports = {
-//     sendBitcoin
-// };
 
 
