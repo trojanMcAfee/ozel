3ff7a5e6db32cbb7f46ab3a12f6027bfdc3850ad
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2022-01-04 14:43:18 +0100

    Working on merge of zmq, redis and bitcoind

--- scripts/listener.js
+++ scripts/listener.js
@@ -4,12 +4,14 @@ const Redis = require('redis');
 const Client = require('bitcoin-core');
 
 const { executeBridge } = require('./exec-bridge.js'); 
+const { raw } = require('body-parser');
 
 require('dotenv').config();
 
 
 const bitPayAddr = 'tb1q0wgk0sf8ucsvh8kmhj4ynnum8pse98mkw45rpl';
-const testAddress = 'mubUbyPazdyvhPJYPGWUkFWj7bkw1Yq8ys';
+const testAddress1 = 'tb1qvdp56fcxth0y3s5qz2rlzhws5n6exsj8pvpq98'; //testWallet_1 balance: 0.00791164 -
+const testAddress = bitPayAddr;
 
 async function run() {
     const bitcoinClient = new Client({ 
@@ -19,20 +21,18 @@ async function run() {
         port: 18332 
     });
 
-    //after solving the address gap limit, create an address within my wallet, send btc to it (save it redis), parse with zeromq
-    //and move the btc to renVM ----> basically do a simulation of the app
-
     const checkOnRedis = async (address) => await redisClient.get(address);
     
     const redisClient = Redis.createClient();
     redisClient.on('error', (err) => console.log('Redis client error ', err));
     await redisClient.connect();
-    if (!checkOnRedis(testAddress)) {
+    if (!await checkOnRedis(testAddress)) {
         await redisClient.set(testAddress, 1);
         console.log(testAddress + ' saved on Redis');
     }
 
-    sendBitcoinCore(testAddress);
+    // sendBitcoinCore(testAddress);
+    // const txid = await bitcoinClient.sendToAddress(testAddress, 0.0001);
 
     const sock = new zmq.Subscriber;
     sock.connect("tcp://127.0.0.1:29000");
@@ -49,9 +49,18 @@ async function run() {
                 address1 = bitcoin.address.fromOutputScript(tx.outs[0].script, bitcoin.networks.testnet); 
                 address2 = bitcoin.address.fromOutputScript(tx.outs[1].script, bitcoin.networks.testnet);
             } catch(e) {}
-            if (( testAddress === address1 || testAddress === address2 ) && checkOnRedis(testAddress)) {
+            if (( testAddress === address1 || testAddress === address2 ) && await checkOnRedis(testAddress)) {
                 const matchingAddress = testAddress === address1 ? address1 : address2;
                 const txid = tx.getId();
+
+                let raw;
+                setInterval(function() {
+                    raw = bitcoinClient.getRawTransaction(txid);
+                    if (raw.confirmations === 1) {
+                        
+                    }
+                }, 120000);
+
                 console.log('Tx Hash Id: ', txid);
                 console.log('address: ', matchingAddress);
             }
