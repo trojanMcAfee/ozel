11e736b201796f470ce82e8a186e9e91d4b54ec3
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2022-09-10 14:57:16 +0200

    Refactored

--- scripts/event-listener.js
+++ scripts/event-listener.js
@@ -6,20 +6,22 @@ async function main() {
     // const URL = (taskId) => `https://api.thegraph.com/subgraphs/name/gelatodigital/poke-me-rinkeby/graphql?query=+query+executions%7B%0A++++++++tasks%28where%3A+%7Bid%3A+%22${taskId}%22%7D%29+%7B%0A++++++++++id%0A++++++++++taskExecutions+%7B%0A%09%09%09%09%09%09id%2C%0A++++++++++++executedAt%0A++++++++++++success%0A++++++++++%7D%0A++++++++%7D%0A++++++%7D`;
     const URL = 'https://api.thegraph.com/subgraphs/name/gelatodigital/poke-me-rinkeby';
     const query = (taskId) => {
-        query: `
-            {
-                executions{
-                    tasks(where: {id: "${taskId}"}) {
+        return {
+            query: `
+                {
+                    executions{
+                        tasks(where: {id: ${taskId}}) {
                         id
                         taskExecutions {
                             id,
                             executedAt
                             success
                         }
+                        }
                     }
                 }
-            }
-        `
+            `
+        }
     };
     const storageBeaconAddr = '0xa6aA583E1Ab33F9E7ED99560e1dfD211332F7FbB';
     const emitterAddr = '0xC06D6180e0387804bbcd36372F80D63B05144073';
@@ -46,24 +48,7 @@ async function main() {
         console.log('');
         // console.log('URL: ', query(taskId));
 
-        const result = await axios.post(URL,
-            {
-                query: `
-                    {
-                        executions{
-                            tasks(where: {id: ${taskId}}) {
-                              id
-                              taskExecutions {
-                                            id,
-                                executedAt
-                                success
-                              }
-                            }
-                          }
-                    }
-                `
-            }
-        );
+        const result = await axios.post(URL, query(taskId));
         console.log('result: ', result);
     });
 
