2479e3cea6a115d5f17d3aade01a7e362695597a
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2022-09-12 13:27:33 +0200

    Done with listener for failed bridge txs (Gelato)

--- scripts/event-listener.js
+++ scripts/event-listener.js
@@ -12,7 +12,6 @@ async function main() {
                         id
                         taskExecutions {
                             id,
-                            executedAt
                             success
                         }
                     }
@@ -23,6 +22,7 @@ async function main() {
     const storageBeaconAddr = '0xD7F4e37632573455F48Ea1E468da0E6C6a4837Ff'; 
     const storageBeacon = await hre.ethers.getContractAt('StorageBeacon', storageBeaconAddr);
     const proxy = '0x68F6059648048f24EB4c2f1b2b9606A716805B88';
+    const failedHashes = [];
 
     const filter = {
         address: proxy, 
@@ -37,8 +37,17 @@ async function main() {
         let [ proxy ] = abiCoder.decode(['address'], codedProxy);
         const taskId = await storageBeacon.getTaskID(proxy);
 
-        const result = await axios.post(URL, query(taskId));
-        console.log('result: ', result.data.data);
+        const result = (await axios.post(URL, query(taskId))).data.data;
+        const executions =  result.tasks[0].taskExecutions;
+
+        for (let i=0; i < executions.length; i++) {
+            if (!executions[i].success) {
+                let [ hash ] = executions[i].id.split(':');
+                failedHashes.push(hash);
+            }
+        }
+
+        console.log('failedHashes: ', failedHashes);
     });
 
 }
