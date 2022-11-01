b38668e2c668d8abf4ebd5a478057a5c93fa5328
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2022-11-01 19:07:49 +0100

    Added FakeGoerliOZL.sol

---
+++ contracts/ethereum/FakeGoerliOZL.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.14;
+
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './StorageBeacon.sol';
+
+
+contract FakeGoerliOZL is Ownable {
+
+    address user;
+    address receiver;
+
+    struct MyVars {
+        uint totalVolumeInUSD;
+        uint totalVolumeInETH;
+        uint wethUM;
+        uint valueUM;
+        uint ozlBalance;
+        uint wethUserShare;
+        uint usdUserShare;
+    }
+
+    MyVars vars;
+
+    constructor(address receiver_, MyVars memory vars_) {
+        receiver = receiver_;
+        vars = MyVars({
+            totalVolumeInUSD: vars_.totalVolumeInUSD,
+            totalVolumeInETH: vars_.totalVolumeInETH,
+            wethUM: vars_.wethUM,
+            valueUM: vars_.valueUM,
+            ozlBalance: vars_.ozlBalance,
+            wethUserShare: vars_.wethUserShare,
+            usdUserShare: vars_.usdUserShare
+        });
+    }
+
+    receive() external payable {}
+
+
+    function getTotalVolumeInUSD() external view returns(uint) {
+        return vars.totalVolumeInUSD;
+    }
+
+    function getTotalVolumeInETH() external view returns(uint) {
+        return vars.totalVolumeInETH;
+    }
+
+    function getAUM() external view returns(uint, uint) {
+        return (vars.wethUM, vars.valueUM);
+    }
+
+    function balanceOf(address user_) external view returns(uint) {
+        return vars.ozlBalance;
+    }
+
+    function getOzelBalances(
+        address user_
+    ) external view returns(uint wethUserShare, uint usdUserShare) {
+        return (vars.wethUserShare, vars.usdUserShare);
+    }
+
+    //----------
+
+    function exchangeToUserToken(StorageBeacon.UserConfig memory userDetails_) external payable {
+        if (address(this).balance > 0) {
+            (bool success, ) = receiver.call{value: address(this).balance}(""); 
+            require(success, 'ETH sent failed');
+        }
+        user = userDetails_.user;
+    }
+
+    function changeMyVars(MyVars memory newVars_) external onlyOwner {
+        vars = newVars_;
+    } 
+
+    function changeReceiver(address newReceiver_) external onlyOwner {
+        receiver = newReceiver_;
+    }
+}
