c72fee8b0c7efcd6aad358e160dffbc3e12167ab
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2022-11-02 21:56:11 +0100

    Before clean up

--- scripts/network.js
+++ scripts/network.js
@@ -1,25 +1,22 @@
 
-//Set of auto-redeem contracts deployed to Goerli and Arb Goerli
-
 // signer address:  0x0E743a1E37D691D8e52F7036375F3D148B4116ba
-// FakeOZL deployed to:  0x543711ED09A2d8a2c34b7599d9e323cd03B05c94
-// Emitter deployed to:  0xF2CBA3Ff01788713dDa2637C87a2B2FFd4365eE9
-// ozPayMe deployed to:  0xCE91A7cA139e778c89F0E319358Aa19f8c3D5754
-// StorageBeacon deployed to:  0x3996522Bc9903f00c4f62aa71CAB48506fd9692D
-// ozUpgradeableBeacon deployed to:  0x48959072D9d770A67E0CA5DCBEFdeb8DE191c697
-// ProxyFactory deployed to:  0x4FECd47761bFb55E981604E4C331f4F8Fa97Df58
-// ozERC1967Proxy deployed to:  0x31ED67cd9F4520c4783DD779cb0E824e61C2B665
-// initialize with hash:  0xb633f5eeb888c0df12b81adf7a7c008499a6c1464361ea4d1e3dde5b5bbd356b
-// RedeemedHashes deployed to:  0x264dA78E44781eCDFdff18e11C5759DcC582B231
-// RolesAuthority deployed to:  0x68F08850f630524C907C771056d809c91BF48460
-// createNewProxy with hash:  0x2a6b7c3ce5343361ea137d7c9c982efa4ed9602eb0ff61255d57ed3d13265d1c
-// proxy 1:  0x0db608Ba3a657Fd4CE9296d45d37AE1D8D80efb3
-// task id:  0x9788ddb664e3ec49dc7aa2b2e8c2532dfc21d38412004bf87f14d383471ad343
-
+// FakeOZL deployed to:  0xB2CfB9e7239e7eFF83D0C730AcFD7a01B76d72f6
+// Emitter deployed to:  0xF41DDB1C1bc77B9778b4e3e0985919653a0DB268
+// ozPayMe deployed to:  0x44C9c02BAc24a1Ded45Ccb4b0eBdFB72645bAb21
+// StorageBeacon deployed to:  0xC745d873ac445b676aDD392aae8E49609282273E
+// ozUpgradeableBeacon deployed to:  0x6CE0D1bCf1Bef2dcf9265Ac7b20B91fD951f5E9C
+// ProxyFactory deployed to:  0xd8816CeA49Cfa02e7eD47452AfBa8a70e150c5E6
+// ozERC1967Proxy deployed to:  0xdcB7231B7d287Eac28CAF0014A7f354a9217B8a1
+// initialize with hash:  0x4e6ee16e520d38b6eea15d9ad4ea23d62d61536e6405ec95c4485c755c1ff5e3
+// RedeemedHashes deployed to:  0xC8447274c5BED0044Cd1754160Eb295459290D0D
+// RolesAuthority deployed to:  0xAb3b4552537e383ff538Ab89b53b0EDFaDfb75D7
+// createNewProxy with hash:  0xfe3573bb882879d36347c487287acc3f440d932e9e5b9a8dc9adbca4ea0842ba
+// proxy 1:  0x4Cb12002de75C581863C383c38a74F7BFac75515
+// task id:  0xb41e8b76c281c56904f8df1013c39c9aa76c59810ed35022271d751b158ffb48
 
 
 async function main() {
-    const myAddr = '0x0E743a1E37D691D8e52F7036375F3D148B4116ba'; //main Metamask acc 
+    const myAddr = ''; // <----- connected Metamask account goes here
     const diamondAddr = '0x543711ED09A2d8a2c34b7599d9e323cd03B05c94'; //FakeOZL
     const ozlDiamond = await hre.ethers.getContractAt('FakeOZL', diamondAddr);
 
