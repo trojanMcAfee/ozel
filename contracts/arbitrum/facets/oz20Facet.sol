b593bfc70c23f79dc6403ceb6a3823e94792eb58
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2022-11-30 23:00:40 +0100

    Done with comments on oz20Facet (no Ioz20)

--- contracts/arbitrum/facets/oz20Facet.sol
+++ contracts/arbitrum/facets/oz20Facet.sol
@@ -8,7 +8,6 @@ import '@openzeppelin/contracts/utils/Context.sol';
 import '@openzeppelin/contracts/utils/Address.sol';
 import { LibDiamond } from "../../libraries/LibDiamond.sol";
 import { ModifiersARB } from '../../Modifiers.sol';
-import '../../interfaces/ozI20Facet.sol';
 import '../../Errors.sol';
 
 
@@ -17,13 +16,38 @@ import '../../Errors.sol';
  * @notice A new version was made, instead of inheriting from the original 
  * and overriding methods, since a new storage layout was needed in order  
  * to fit the architecture of the whole system. 
- * @dev Untouched functions have been tagged as "Unchanged method"
  */
-contract oz20Facet is ozI20Facet, ModifiersARB, Context, IERC20, IERC20Metadata {
+contract oz20Facet is ModifiersARB, Context, IERC20, IERC20Metadata {
     
     using FixedPointMathLib for uint;
     using Address for address;
 
+
+    /*///////////////////////////////////////////////////////////////
+                            Unchanged methods
+    //////////////////////////////////////////////////////////////*/
+
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() external pure returns (uint256) { 
+        return 100;
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                    Methods with different storage layout
+    //////////////////////////////////////////////////////////////*/
 
     function name() external view returns (string memory) {
         return s.oz.name;
@@ -33,67 +57,10 @@ contract oz20Facet is ozI20Facet, ModifiersARB, Context, IERC20, IERC20Metadata 
         return s.oz.symbol; 
     }
 
-    /// @dev Unchanged method
-    function decimals() external view returns (uint8) {
-        return 18;
-    }
-
-    /// @dev Unchanged method
-    function totalSupply() external view returns (uint256) { 
-        return 100;
-    }
-
-    /// @inheritdoc ozI20Facet
-    function balanceOf(address user_) public view returns (uint256) { 
-        uint stableMod = s.indexFlag ? 1 : s.stabilizer; 
-        return (s.ozelIndex.mulDivDown(s.usersPayments[user_] * 100, 10 ** 22) / 4 ** s.indexRegulator) / stableMod;
-    }
-
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
-    function transfer(address recipient, uint256 amount) external returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-allowance}.
-     */
     function allowance(address owner, address spender) external view returns (uint256) {
         return s.oz.allowances[owner][spender];
     }
 
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function approve(address spender, uint256 amount) external returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20}.
-     *
-     * Requirements:
-     *
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for ``sender``'s tokens of at least
-     * `amount`.
-     */
     function transferFrom(
         address sender,
         address recipient,
@@ -110,37 +77,11 @@ contract oz20Facet is ozI20Facet, ModifiersARB, Context, IERC20, IERC20Metadata 
         return true;
     }
 
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
         _approve(_msgSender(), spender, s.oz.allowances[_msgSender()][spender] + addedValue);
         return true;
     }
 
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
     function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
         uint256 currentAllowance = s.oz.allowances[_msgSender()][spender];
         require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
@@ -151,19 +92,36 @@ contract oz20Facet is ozI20Facet, ModifiersARB, Context, IERC20, IERC20Metadata 
         return true;
     }
 
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "oz20Facet: approve from the zero address");
+        require(spender != address(0), "oz20Facet: approve to the zero address");
+
+        s.oz.allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Methods with custom logic
+    //////////////////////////////////////////////////////////////*/
+
     /**
-     * @dev Moves `amount` of tokens from `sender` to `recipient`.
-     *
-     * This internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
+     * @notice Queries OZL balance
+     * @dev Rebase mechanism, pegged to the token's total supply (100)  
+     * @param user_ account to be queried
+     * @return uint OZL balance of user_
+     */
+    function balanceOf(address user_) public view returns (uint256) { 
+        uint stableMod = s.indexFlag ? 1 : s.stabilizer; 
+        return (s.ozelIndex.mulDivDown(s.usersPayments[user_] * 100, 10 ** 22) / 4 ** s.indexRegulator) / stableMod;
+    }
+
+    /**
+     * @dev The transfer of OZL is done by modifying the sender and recipient's payments
+     *      done in the system instead of updating a balances mapping.
      */
     function _transfer( 
         address sender,
@@ -189,59 +147,39 @@ contract oz20Facet is ozI20Facet, ModifiersARB, Context, IERC20, IERC20Metadata 
         emit Transfer(sender, recipient, amount);
     }
 
-
+    /**
+     * @dev Modifies several values, at the moment of withdrawing fees, that are 
+     * used for the calculations of the rebase of OZL.
+     * @param user_ User where the withdrawn fees will be redirected to
+     * @param amount_ OZL balance to redeem
+     * @param lockNum_ Index of the bit which authorizes the function call 
+     */
     function burn(
-        address account, 
-        uint amount,
+        address user_, 
+        uint amount_,
         uint lockNum_
     ) external isAuthorized(lockNum_) noReentrancy(4) { 
-        if(account == address(0)) revert CantBeZero('oz4626Facet: address');
+        if(user_ == address(0)) revert CantBeZero('oz4626Facet: address');
 
-        uint256 accountBalance = balanceOf(account); 
-        if(!(accountBalance >= amount)) revert ConditionNotMet("oz20Facet: burn amount exceeds balance");
+        uint256 accountBalance = balanceOf(user_); 
+        if(!(accountBalance >= amount_)) revert ConditionNotMet("oz20Facet: burn amount exceeds balance");
 
-        uint userBalanceOZL = balanceOf(account);
+        uint userBalanceOZL = balanceOf(user_);
         if(!(userBalanceOZL > 0)) revert ConditionNotMet("oz20Facet: userBalanceOZL cannot be 0");
 
-        uint allocationPercentage = (amount.mulDivDown(10000, userBalanceOZL)).mulDivDown(1 ether, 100);
-        uint amountToReduce = allocationPercentage.mulDivDown(s.usersPayments[account], 100 * 1 ether);
+        uint allocationPercentage = (amount_.mulDivDown(10000, userBalanceOZL)).mulDivDown(1 ether, 100);
+        uint amountToReduce = allocationPercentage.mulDivDown(s.usersPayments[user_], 100 * 1 ether);
 
         //Mutex bitmap lock
         _toggleBit(1, 5);
 
         bytes memory data = abi.encodeWithSignature(
             'modifyPaymentsAndVolumeExternally(address,uint256,uint256)', 
-            account, amountToReduce, 5
+            user_, amountToReduce, 5
         );
 
         LibDiamond.callFacet(data);
 
-        emit Transfer(account, address(0), amount);
-    }
-
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
-     *
-     * This internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     */
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual {
-        require(owner != address(0), "oz20Facet: approve from the zero address");
-        require(spender != address(0), "oz20Facet: approve to the zero address");
-
-        s.oz.allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
+        emit Transfer(user_, address(0), amount_);
     }
 }
